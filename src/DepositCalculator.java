aa86caa28755bbf7690dd5b86f4abc17192dd1c1
Anton Galukin <102962150+tonasd@users.noreply.github.com>
2022-10-13 08:46:47 +0300

    Proposed changes are made
    Полностью согласен с твоими замечаниями. Сделал соответствующие исправления.

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -19,22 +19,26 @@ public class DepositCalculator {
     void calculateDeposit() {
         int period;
         int action;
+        int amount;
+        double out = 0; // ++ Как я понял из урока, эту переменную тоже можно переместить вначало метода к другим обяъвлениям переменных
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Введите сумму вклада в рублях:");//Мне кажется, для улучшения читаемости кода можно разделять переменные ввода пустой строкой
-        int amount = scanner.nextInt();
+        amount = scanner.nextInt();
         System.out.println("Введите срок вклада в годах:") ;
         period = scanner.nextInt();
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
         action = scanner.nextInt();
-        double out = 0;//Как я понял из урока, эту переменную тоже можно переместить вначало метода к другим обяъвлениям переменных
+        
         if (action == 1) {
             out = calculateSimplePercent(amount, 0.06, period);
         } else if (action == 2) {
             out = calculateComplexPercent(amount, 0.06, period);
         }
         System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + out);//Думаю, читабельность кода улучшится, если отделить строку вывода пустой строкой
-    }//И здесь я бы поставил пустую строчку
+    }
+    
+    // ++ И здесь я бы поставил пустую строчку
     public static void main(String[] args) {
         new DepositCalculator().calculateDeposit();
     }
