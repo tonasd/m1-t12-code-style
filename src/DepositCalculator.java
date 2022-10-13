67850f742f843b79ac1cfaecc3d1d6cbe6a028de
Anton Galukin <102962150+tonasd@users.noreply.github.com>
2022-10-13 08:29:03 +0300

    Update DepositCalculator.java
    Привет. Все пробелы на месте, всё по строкам хорошо выровнено. Все скобки на местах. Молодец.
    Теперь к моментам которые желательно исправить. Все комментарии оставлены мною в коде. В общем, возможно, тебе тяжело общаться с английскими терминами и из-за этого тебе не бросаются в глаза несоответствия. Но в общем в лекции был перечень советов как их оптимизировать, ты часть сделал (rnd - round), а часть пропустил...
    Это если что здесь перед директивами import: https://practicum.yandex.ru/learn/java-developer/courses/e59bc09f-08e7-4e38-ad4b-e1f3466caa68/sprints/75167/topics/1be24530-f4b2-4214-89b5-851c13005fbf/lessons/fd67b0eb-b152-4a88-aa43-08d115dc20d5/
    
    Ну и есть одно критическое замечание, у тебя пункт 1 меню отрабатывает неверно, он считает сложный процент хотя должен считать простой.

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -1,28 +1,31 @@
 import java.util.Scanner;
 
 public class DepositCalculator {
-    double calculateComplexPercent(double a, double y, int d) {
+    // Параметры метода не информативны, что нужно класть в a, что в y и d ?
+    double calculateComplexPercent(double a, double y, int d) { 
         double pay = a * Math.pow((1 + y / 12), 12 * d);
         return round(pay, 2);
     }
-
-    double calculateSimplePercent(double doubleAmount, double double_year_rate, int deposit_period) {
+    // Этот метод и метод выше принимают одинаковые аргументы, можно взять параметры этого метода за образец названия предыдущего
+    // Параметры метода не соответствуют lowerCamelCase, некоторые содержат названия типов
+    double calculateSimplePercent(double doubleAmount, double double_year_rate, int deposit_period) { 
         return round(doubleAmount + doubleAmount * double_year_rate * deposit_period, 2);
     }
-
+    // здесь scaLe сбивает с толку, поскольку это не два объединённых слова, а одно. scale - так будет лучше
     double round(double value, int places) {
         double scaLe = Math.pow(10, places);
         return Math.round(value * scaLe) / scaLe;
     }
-
+    /* Имя метода не информативно. Это основной метод класса. Он рассчитывает размер депозита при применении сложного или простого процента по выбору 
+    пользователя. Соответственно он рассчитывает(calculate) что будетс депозитом (Deposit) = calculateDeposit (либо просто calculate) */
     void doImportantJob() {
         int period;
         int action;
         double out = 0.0;
-        Scanner abcdef = new Scanner(System.in);
+        Scanner abcdef = new Scanner(System.in); // abcdef - не информативно, лучше назвать так-же как у нас называется этот объект на протяжении курса
 
         System.out.println("Введите сумму вклада в рублях:");
-        int amount = abcdef.nextInt();
+        int amount = abcdef.nextInt(); // возможно объявление переменной лучше вынести вверх, к остальным
 
         System.out.println("Введите срок вклада в годах:");
         period = abcdef.nextInt();
@@ -30,8 +33,8 @@ public class DepositCalculator {
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
         action = abcdef.nextInt();
 
-        if (action == 1) {
-            out = calculateComplexPercent(amount, 0.06, period);
+        if (action == 1) { // Не сходится пункт меню и применённый метод, тут нужен метод для рассчёта простого процента
+            out = calculateComplexPercent(amount, 0.06, period); 
         } else if (action == 2) {
             out = calculateComplexPercent(amount, 0.06, period);
         }
