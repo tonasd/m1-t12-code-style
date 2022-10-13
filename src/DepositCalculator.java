6472f31c8f05f563b733ffc8e73f289430d66d6a
Anton Galukin <102962150+tonasd@users.noreply.github.com>
2022-10-13 09:06:08 +0300

    Update DepositCalculator.java

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -19,22 +19,27 @@ public class DepositCalculator {
     void calculateDeposit() {
         int period;
         int action;
+        int amount;
+        double out = 0;
+        
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Введите сумму вклада в рублях:");
-        int amount = scanner.nextInt();
+        amount = scanner.nextInt();
         System.out.println("Введите срок вклада в годах:") ;
         period = scanner.nextInt();
         System.out.println("Выберите тип вклада, 1 - вклад с обычным процентом, 2 - вклад с капитализацией:");
         action = scanner.nextInt();
-        double out = 0;
+        
         if (action == 1) {
             out = calculateSimplePercent(amount, 0.06, period);
         } else if (action == 2) {
             out = calculateComplexPercent(amount, 0.06, period);
         }
+        
         System.out.println("Результат вклада: " + amount + " за " + period + " лет превратятся в " + out);
     }
+    
     public static void main(String[] args) {
         new DepositCalculator().calculateDeposit();
     }
